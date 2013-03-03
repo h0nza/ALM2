5afb09017d2a74f4fc3920dafb8e6f0a9e69b99f
Eliska Vlckova <efox.vlckova@gmail.com>
2013-03-03 22:08:52 +0100

    nejake upravy C++ -> C

--- alm2_02_vyhledavaci_stromy_priklady/bst_string.c
+++ alm2_02_vyhledavaci_stromy_priklady/bst_string.c
@@ -2,52 +2,11 @@
 //
 
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
-typedef int Item;
-typedef struct Nod
-{
-	Item item;
-	Nod*left;
-	Nod*right;
-}Node;
+#include "bst_string.h"
 
-Node *Search(Node *root, Item x);
-bool Insert(Node **root, Item x);
-bool Delete(Node **root, Item x);
-bool Print(Node *root);
 
-int main()
-{
-	Node *r=NULL;
-	Insert(&r,6);
-	Insert(&r,5);
-	Insert(&r,7);
-	Insert(&r,4);
-	Insert(&r,8);
-	Insert(&r,3);
-	Insert(&r,9);
-	Insert(&r,2);
-	Insert(&r,10);
-	Insert(&r,1);
-	Print(r);
-	printf("\n");
-	Node *s=Search(r,4);
-	if(s==NULL) printf("Uzel nenalezen.\n"); else printf("Uzel nalezen.\n");
-	s=Search(r,12);
-	if(s==NULL) printf("Uzel nenalezen.\n"); else printf("Uzel nalezen.\n");
-	Delete(&r,4);
-	Print(r);
-	printf("\n");
-	Delete(&r,1);
-	Print(r);
-	printf("\n");
-	Delete(&r,6);
-	Print(r);
-	printf("\n");
-	system("PAUSE");
-	return 0;
-}
 
 Node *Search(Node *root, Item x)
    { 
