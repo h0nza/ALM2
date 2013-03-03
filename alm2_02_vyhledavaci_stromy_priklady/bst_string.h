4247ed6542e13c43808f04ebf4b4dcce2a0ae254
Eliska Vlckova <efox.vlckova@gmail.com>
2013-03-03 22:06:50 +0100

    nejake upravy z C++ do C, pridan Makefile a bst_string.h

---
+++ alm2_02_vyhledavaci_stromy_priklady/bst_string.h
@@ -0,0 +1,15 @@
+
+#include <stdbool.h>
+
+typedef int Item;
+typedef struct Node
+{
+	Item item;
+	struct Node *left;
+	struct Node *right;
+} Node;
+
+Node *Search(Node *root, Item x);
+bool Insert(Node **root, Item x);
+bool Delete(Node **root, Item x);
+bool Print(Node *root);
