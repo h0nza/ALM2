0de3ed407a4cb3a99a7cd2f53d629c194ca26e5c
Jan Vlcek <j4n.v1cek@gmail.com>
2013-03-05 19:34:47 +0100

    upraven styl kodu podle http://tux.inf.upol.cz/~havrlant/vyuka/2012/zp1/konvence.php

--- alm2_02_vyhledavaci_stromy_priklady/bst_string.c
+++ alm2_02_vyhledavaci_stromy_priklady/bst_string.c
@@ -8,44 +8,57 @@
 
 
 
-Node *Search(Node *root, Item x)
-   { 
+Node *Search(Node *root, Item x) { 
      Node *u=root;
-     while (u != NULL)
-     { if (x < u->item) u=u->left;
-       else if (x > u->item) u=u->right;
+     while (u != NULL) {
+       if (x < u->item) {
+          u=u->left;
+       }
+       else if (x > u->item) {
+          u=u->right;
+       }
        else return u;
      }
      return NULL;
-   }
+}
 
-bool Insert(Node **root, Item x)
-   { 
+bool Insert(Node **root, Item x) { 
      Node **u=root;
-     while (*u != NULL)
-     { if (x < (*u)->item) u=&(*u)->left;
-       else if (x > (*u)->item) u=&(*u)->right;
-       else return false;
+     while (*u != NULL) {
+       if (x < (*u)->item) {
+          u=&(*u)->left;
+       }
+       else if (x > (*u)->item) {
+          u=&(*u)->right;
+       }
+       else
+          return false;
      }
      { Node *v= (Node *)malloc(sizeof(Node));
        v->item = x;
        v->left = v->right = NULL;
        *u = v;     }  
      return true;
-   }
+}
 
-bool Delete(Node **root, Item x)
-  { 
+bool Delete(Node **root, Item x) { 
     Node **u=root;
-    while (*u != NULL)
-    { if (x < (*u)->item) { u=&(*u)->left; continue; }
-      if (x == (*u)->item) break;
+    while (*u != NULL) {
+      if (x < (*u)->item) {
+         u=&(*u)->left;
+         continue;
+      }
+      if (x == (*u)->item)
+         break;
       u=&(*u)->right;
     }
-    if (*u==NULL) return false;
-    if ((*u)->left!=NULL && (*u)->right!=NULL)
-    { Node **v=&(*u)->right;
-      while ((*v)->left!=NULL) v=&(*v)->left; 
+    if (*u==NULL)
+       return false;
+    if ((*u)->left != NULL && (*u)->right != NULL) {
+      Node **v=&(*u)->right;
+      while ((*v)->left!=NULL) {
+        v=&(*v)->left;
+      }
       (*u)->item=(*v)->item;
       u=v;
     }
@@ -53,16 +66,13 @@ bool Delete(Node **root, Item x)
       *u= v->left ? v->left : v->right;
       free(v);//delete v;
     } 
-  }
-
-bool Print(Node *root)
-{
-	if(root!=NULL) 
-	{
-		Print(root->left);
-		printf("%s ",root->item);
-		Print(root->right);
-	}
-	return true;
 }
 
+bool Print(Node *root) {
+  if (root != NULL) {
+     Print(root->left);
+     printf("%s ",root->item);
+     Print(root->right);
+  }
+  return true;
+}
